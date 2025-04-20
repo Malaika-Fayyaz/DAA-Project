eaf9e22b5c7f666a097703da483699a794e7cbe4
Malaika-Fayyaz <149476432+Malaika-Fayyaz@users.noreply.github.com>
2025-04-20 19:02:39 +0500

    linux compatible now

--- server1.c
+++ server1.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <unistd.h>
+#include <string.h>
 #include <stdbool.h>
-#include <winsock2.h>
-#include <windows.h>
+#include <time.h>
 #include <math.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
-#define PORT 9999  // Port for the first server
+#define PORT 9999
 
-
-
-// Miller-Rabin Primality Test (for 32-bit integers)
 bool miller_rabin(int n, int k) {
     if (n <= 1 || n == 4) return false;
     if (n <= 3) return true;
@@ -24,9 +24,7 @@ bool miller_rabin(int n, int k) {
         int x = 1;
         int temp = d;
 
-        // Compute a^d % n
-        long long res = 1;
-        long long base = a;
+        long long res = 1, base = a;
         while (temp > 0) {
             if (temp % 2 == 1)
                 res = (res * base) % n;
@@ -35,8 +33,7 @@ bool miller_rabin(int n, int k) {
         }
         x = (int)res;
 
-        if (x == 1 || x == n - 1)
-            continue;
+        if (x == 1 || x == n - 1) continue;
 
         bool continue_outer = false;
         for (int r = 1; r <= log2(n - 1); r++) {
@@ -46,9 +43,7 @@ bool miller_rabin(int n, int k) {
                 break;
             }
         }
-
-        if (continue_outer)
-            continue;
+        if (continue_outer) continue;
 
         return false;
     }
@@ -56,72 +51,49 @@ bool miller_rabin(int n, int k) {
     return true;
 }
 
-
 int generate_prime() {
-    srand(time(NULL) ^ GetCurrentProcessId());
+    srand(time(NULL) ^ getpid());
     int prime;
     while (1) {
         prime = rand() % (999999 - 10000 + 1) + 10000;
-        if (prime % 2 == 0) prime++; // Ensure odd number
-        if (miller_rabin(prime, 5)) // 5 rounds for reasonable certainty
+        if (prime % 2 == 0) prime++;
+        if (miller_rabin(prime, 5))
             break;
     }
     return prime;
 }
 
-
-
-
 int main() {
-    WSADATA wsaData;
-    SOCKET server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    int addr_len = sizeof(client_addr);
+    int server_fd, client_socket;
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(address);
 
-    // Initialize Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        printf("WSAStartup failed\n");
-        return 1;
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
     }
 
-    // Create socket
-    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
-        printf("Socket creation failed\n");
-        WSACleanup();
-        return 1;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
     }
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-
-    // Bind the socket to the port
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
-        printf("Bind failed\n");
-        closesocket(server_socket);
-        WSACleanup();
-        return 1;
-    }
-
-    // Listen for incoming connections
-    if (listen(server_socket, 3) == SOCKET_ERROR) {
-        printf("Listen failed\n");
-        closesocket(server_socket);
-        WSACleanup();
-        return 1;
+    if (listen(server_fd, 3) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
     }
 
     printf("Server 1 listening on port %d...\n", PORT);
 
-    // Accept incoming connection
-    if ((client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_len)) == INVALID_SOCKET) {
-        printf("Accept failed\n");
-        closesocket(server_socket);
-        WSACleanup();
-        return 1;
+    if ((client_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0) {
+        perror("accept failed");
+        exit(EXIT_FAILURE);
     }
 
-    // Generate and send the prime number
     int prime = generate_prime();
     printf("Server 1 generated prime: %d\n", prime);
 
@@ -131,10 +103,7 @@ int main() {
 
     printf("Prime sent to client.\n");
 
-    // Cleanup
-    closesocket(client_socket);
-    closesocket(server_socket);
-    WSACleanup();
-
+    close(client_socket);
+    close(server_fd);
     return 0;
 }
