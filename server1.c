3afec392b4b5281e944e730debd24f3ed4e633d1
Malaika-Fayyaz <149476432+Malaika-Fayyaz@users.noreply.github.com>
2025-04-24 21:28:00 +0500

    prime length changed to 100 digits

--- server1.c
+++ server1.c
@@ -2,65 +2,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <stdbool.h>
 #include <time.h>
-#include <math.h>
+#include <gmp.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
 #define PORT 9999
+#define PRIME_DIGITS 100
 
-bool miller_rabin(int n, int k) {
-    if (n <= 1 || n == 4) return false;
-    if (n <= 3) return true;
+void generate_prime(mpz_t prime) {
+    gmp_randstate_t state;
+    gmp_randinit_mt(state);
+    gmp_randseed_ui(state, time(NULL) ^ getpid());
 
-    int d = n - 1;
-    while (d % 2 == 0)
-        d /= 2;
+    do {
+        mpz_urandomb(prime, state, PRIME_DIGITS * 3.32193); // bits ≈ digits * log2(10)
+        mpz_nextprime(prime, prime);
+    } while (mpz_sizeinbase(prime, 10) != PRIME_DIGITS);  // Ensure 100-digit length
 
-    for (int i = 0; i < k; i++) {
-        int a = 2 + rand() % (n - 4);
-        int x = 1;
-        int temp = d;
-
-        long long res = 1, base = a;
-        while (temp > 0) {
-            if (temp % 2 == 1)
-                res = (res * base) % n;
-            base = (base * base) % n;
-            temp /= 2;
-        }
-        x = (int)res;
-
-        if (x == 1 || x == n - 1) continue;
-
-        bool continue_outer = false;
-        for (int r = 1; r <= log2(n - 1); r++) {
-            x = (int)(((long long)x * x) % n);
-            if (x == n - 1) {
-                continue_outer = true;
-                break;
-            }
-        }
-        if (continue_outer) continue;
-
-        return false;
-    }
-
-    return true;
-}
-
-int generate_prime() {
-    srand(time(NULL) ^ getpid());
-    int prime;
-    while (1) {
-        prime = rand() % (999999 - 10000 + 1) + 10000;
-        if (prime % 2 == 0) prime++;
-        if (miller_rabin(prime, 5))
-            break;
-    }
-    return prime;
+    gmp_randclear(state);
 }
 
 int main() {
@@ -87,23 +48,27 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    printf("Server 1 listening on port %d...\n", PORT);
+    printf("Server listening on port %d...\n", PORT);
 
     if ((client_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0) {
         perror("accept failed");
         exit(EXIT_FAILURE);
     }
 
-    int prime = generate_prime();
-    printf("Server 1 generated prime: %d\n", prime);
+    mpz_t prime;
+    mpz_init(prime);
+    generate_prime(prime);
 
-    char prime_str[12];
-    sprintf(prime_str, "%d", prime);
+    char *prime_str = mpz_get_str(NULL, 10, prime);
+    printf("Generated 100-digit prime:\n%s\n", prime_str);
+
     send(client_socket, prime_str, strlen(prime_str), 0);
-
     printf("Prime sent to client.\n");
 
+    free(prime_str);
+    mpz_clear(prime);
     close(client_socket);
     close(server_fd);
+
     return 0;
 }
